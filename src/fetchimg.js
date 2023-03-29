324f716879f993c21d0b4d68fd46c91c9338fe34
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-29 23:18:35 +0200

    vse cshe kryvo

--- src/fetchimg.js
+++ src/fetchimg.js
@@ -1,30 +1,30 @@
 const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // import {searchQuery} from './index.js';
 
-const API_KEY = "34815757-11540e8253cc9068e0c598d0d";    
-let searchQuery = "";
+const API_KEY = '34815757-11540e8253cc9068e0c598d0d';
+let searchQuery = '';
 let currentPage = 1;
 let onPage = 8;
 export default async function getImages(ev) {
- 
-    searchQuery = ev.target.elements.searchQuery.value.toLowerCase().trim();
-    // console.log(searchQuery);
-    try {
-      const listImages = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${onPage}`);
-      console.log(listImages);
-      
-      if(searchQuery = ""){
-        return Notify.failure('Please enter another search query');
-      }
-      currentPage += 1;
-             return listImages;
-       } catch (error) {
-      console.error(error);
-      Notify.warning(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }}
+  searchQuery = ev.target.elements.searchQuery.value.toLowerCase().trim();
+  // console.log(searchQuery);
+  try {
+    const listImages = await axios.get(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${onPage}`
+    );
+    console.log(listImages);
 
-
+    if ((searchQuery = "")) {
+      return Notify.failure('Please enter another search query');
+    }
+    currentPage += 1;
+    return listImages;
+  } catch (error) {
+    console.error(error);
+    Notify.warning(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 import './fetchimg.js';
 const axios = require('axios').default;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import getImages from './fetchimg.js';
 
-const API_KEY = "34815757-11540e8253cc9068e0c598d0d";
+const API_KEY = '34815757-11540e8253cc9068e0c598d0d';
 // let searchQuery = "";
 let backup = ``;
 const form = document.querySelector('#search-form');
 const inputGet = form.querySelector('input');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-loadMoreBtn.classList.toggle("is-hidden");
+loadMoreBtn.classList.toggle('is-hidden');
 
-form.addEventListener("submit", renderImages);
+form.addEventListener('submit', renderImages);
 
- function renderImages(ev){
+function renderImages(ev) {
   ev.preventDefault();
   currentPage = 1;
- 
-  
-  getImages(ev).then( response => { 
+
+  getImages(ev).then(response => {
     console.log(response);
 
-    gallery.innerHTML = "";
+    gallery.innerHTML = '';
 
-    if(response.totalHits === 0){
+    if (response.data.totalHits === 0) {
       Notify.warning(
         'Sorry, there are no images matching your search query. Please try again.'
       );
-    }else {Notify.success(`Hooray! We found ${response.data.totalHits} images`);}
-  response.data.hits.forEach(item => {
-    console.log(item);
-     backup += `<div class="photo-card">
-    <a class="gallery__item" href=${item.largeImageURL}><img class="gallery__image" src=${item.previewURL
-    } alt=${item.tags} loading="lazy"/></a>
+    } else {
+      Notify.success(`Hooray! We found ${response.data.totalHits} images`);
+    }
+    response.data.hits.forEach(item => {
+      // console.log(item);
+      backup += `<div class="photo-card">
+    <a class="gallery__item" href=${item.largeImageURL}><img class="gallery__image" src=${item.previewURL} alt=${item.tags} loading="lazy"/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${item.likes}</b>
@@ -50,38 +50,39 @@ form.addEventListener("submit", renderImages);
       <b>Downloads: ${item.downloads}</b>
     </p>
   </div>
-</div>`
-  })
-  gallery.insertAdjacentHTML('afterbegin', backup);
-  
-  loadMoreBtn.classList.toggle("is-hidden");
-    
-  const lightbox = new SimpleLightbox('.gallery a', {
-    // captions: true,
-    // captionsData: 'alt',
-    captionDelay: 250,
-  });})}
+</div>`;
+    });
+    gallery.insertAdjacentHTML('afterbegin', backup);
 
-  loadMoreBtn.addEventListener("click", onLoadMore)
+    loadMoreBtn.classList.toggle('is-hidden');
 
-  function onLoadMore(ev) {
-    ev.preventDefault();
-    backup = ``;
-    
-    getImages(ev).then( response => { 
-      console.log(response)
-      if(response.data.totalHits === 0){
-        Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      };
-      if(response.data.totalHits <= (onPage * currentPage)){
-        loadMoreBtn.classList.toggle("is-hidden");
-      }
+    const lightbox = new SimpleLightbox('.gallery a', {
+      // captions: true,
+      // captionsData: 'alt',
+      captionDelay: 250,
+    });
+  });
+}
+
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onLoadMore(ev) {
+  ev.preventDefault();
+  backup = ``;
+
+  getImages(ev).then(response => {
+    console.log(response);
+    if (response.data.totalHits === 0) {
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    if (response.data.totalHits <= onPage * currentPage) {
+      loadMoreBtn.classList.toggle('is-hidden');
+    }
     response.data.hits.forEach(item => {
-             backup += `<div class="photo-card">
-      <a class="gallery__item" href=${item.largeImageURL}><img class="gallery__image" src=${item.previewURL
-      } alt=${item.description} loading="lazy"/></a>
+      backup += `<div class="photo-card">
+      <a class="gallery__item" href=${item.largeImageURL}><img class="gallery__image" src=${item.previewURL} alt=${item.description} loading="lazy"/></a>
     <div class="info">
       <p class="info-item">
         <b>Likes: ${item.likes}</b>
@@ -96,13 +97,13 @@ form.addEventListener("submit", renderImages);
         <b>Downloads: ${item.downloads}</b>
       </p>
     </div>
-  </div>`
-    })
+  </div>`;
+    });
     gallery.insertAdjacentHTML('afterbegin', backup);
     const lightbox = new SimpleLightbox('.gallery a', {
       // captions: true,
       // captionsData: 'alt',
       captionDelay: 250,
-    })})}
-  
-
+    });
+  });
+}
